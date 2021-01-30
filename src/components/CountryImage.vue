bca8a21e428ecceef0381af6c3c4dc2fa9f4653a
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2021-01-31 01:00:49 +0530

    added country images

--- src/components/CountryImage.vue
+++ src/components/CountryImage.vue
@@ -26,6 +26,7 @@ export default {
     },
     methods:{
         updateImages: async function(){
+            this.images = ['https://media.gettyimages.com/photos/corona-virus-picture-id1212213054'];
             var tempData = await axios.get("https://pixabay.com/api/?q=" + this.countryName + "&key=" + process.env.VUE_APP_IMAGE_API_KEY);
             tempData.data.hits.forEach((element) => {
                 this.images.push(element.largeImageURL);
